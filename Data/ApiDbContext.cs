71d5d6ff371f526bae6223e6e4a96d5b4bd206e3
p-cuadros <patrick_jcq@hotmail.com>
2022-03-20 07:09:24 -0500

    adding persistence with sqlite

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TodoMinApi.Domain;
+
+namespace TodoMinApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public virtual DbSet<Item> Items {get;set;}
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options){ }        
+    }
+}
--- Readme.md
+++ Readme.md
@@ -3,3 +3,8 @@
 Using a dictionary as repository in memory
 
 Documentation URL/swagger
+
+<h2>Persistence with EFcore and SQLite</h2>
+
+''' dotnet ef migrations add "initial migration"
+''' dotnet ef database update
